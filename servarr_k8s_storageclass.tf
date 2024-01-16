d5d89e41e68f8145275528cc67f7c9767f705c61
Nick Eldridge <nicholas.eldridge@gmail.com>
2024-01-16 09:35:38 -0500

    never commit what you think will work before applying. Fixed nfs media storage classes to reconcile w/ what's running

--- servarr_k8s_storageclass.tf
+++ servarr_k8s_storageclass.tf
@@ -1,13 +1,14 @@
 resource "kubernetes_storage_class" "nfs_media_library" {
   metadata {
-    name = "nfs-${module.servarr.media_library_name}"
+    name = "nfs-${module.servarr_vars.media_library_name}"
   }
-  storage_provisioner = "nfs.csi.k8s.io"
-  reclaim_policy      = "Delete"
-  volume_binding_mode = "Immediate"
+  storage_provisioner    = "nfs.csi.k8s.io"
+  reclaim_policy         = "Delete"
+  volume_binding_mode    = "Immediate"
+  allow_volume_expansion = false
   parameters = {
-    server = module.servarr.nfs_server
-    share  = module.servarr.media_library_path
+    server = module.servarr_vars.nfs_server
+    share  = trimsuffix(module.servarr_vars.media_library_path, "/")
   }
   mount_options = [
     "hard",
@@ -17,14 +18,15 @@ resource "kubernetes_storage_class" "nfs_media_library" {
 
 resource "kubernetes_storage_class" "nfs_media_downloads" {
   metadata {
-    name = "nfs-${module.servarr.media_downloads_name}"
+    name = "nfs-${module.servarr_vars.media_downloads_name}"
   }
-  storage_provisioner = "nfs.csi.k8s.io"
-  reclaim_policy      = "Delete"
-  volume_binding_mode = "Immediate"
+  storage_provisioner    = "nfs.csi.k8s.io"
+  reclaim_policy         = "Delete"
+  volume_binding_mode    = "Immediate"
+  allow_volume_expansion = false
   parameters = {
-    server = module.servarr.nfs_server
-    share  = module.servarr.media_downloads_path
+    server = module.servarr_vars.nfs_server
+    share  = trimsuffix(module.servarr_vars.media_downloads_path, "/")
   }
   mount_options = [
     "hard",
